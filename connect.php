76753116891de1735f95e8a0f8bc1e5562ca3466
ertibeqiri1 <eb70067@ubt-uni.net>
2025-01-27 22:46:12 +0100

    php login

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+$host="localhost";
+$user="root";
+$pass="";
+$db="login";
+$conn=new mysqli($host,$user,$pass,$db);
+if($conn->connect_error){
+    echo "Failed to connect to DataBase".$conn->connect_error;
+}
+
+
+
+
+
+
+
+
+?>
