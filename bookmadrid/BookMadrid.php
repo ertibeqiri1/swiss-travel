28092b011d5cc58ccf18d1159b12b00d3e5f8525
ertibeqiri1 <eb70067@ubt-uni.net>
2025-02-02 20:11:49 +0100

    modifikime navigimi

--- bookmadrid/BookMadrid.php
+++ bookmadrid/BookMadrid.php
@@ -53,7 +53,7 @@
   <div class="container">
     <form action="create.php" method="post">
     <h1>Make a Reservation</h1>
-        <input type="text" name="name" placeholder="Name" required>
+       <input type="text" name="name" placeholder="Name" required>
         <input type="text" name="surname" placeholder="Surname" required>
         <input type="email" name="email" placeholder="Email" required>
         <input type="date" name="reservation_date" required>
